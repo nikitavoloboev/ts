732c9ba49e741254ebda9514f440fd84a9e46f0c
nikitavoloboev <nikita.voloboev@gmail.com>
2016-08-01 15:36:11 +0200

    :sparkles:

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,17 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
-  hs.alert.show("hello there")
-end)
+-- is good to have for the cli utility I use to reload config
+require("hs.ipc")
 
+-- reload config from a hotkey
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
   hs.reload()
 end)
+
+-- show my todo task in a neat window briefly
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
+hs.alert.show( ( hs.execute("~/Documents/app/bitbar/todo") ) )
+end)
+
+-- not sure about this
 hs.alert.show("config loaded")
+
+
