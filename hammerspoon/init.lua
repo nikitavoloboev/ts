4bb142dcc8f33d8182bc5243612433a82c600ac2
nikitavoloboev <nikita.voloboev@gmail.com>
2016-08-03 22:46:33 +0200

    :sparkles:

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,17 @@
--- is good to have for the cli utility I use to reload config
+--------------------------------------------------------------------------------
+-- -constants
+--------------------------------------------------------------------------------
+local cmd_alt = {"cmd", "alt"}
+local cmd_alt_ctrl = {"cmd", "alt", "ctrl"}
+local main_monitor = "Color LCD"
+local second_monitor = "DELL U2515H"
+
+hs.window.animationDuration = 0 
+
+-- needed for cli utility I use to reload config
 require("hs.ipc")
 
--- reload config from a hotkey
+-- reload config from hotkey
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
   hs.reload()
 end)
@@ -11,7 +21,21 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
 hs.alert.show( ( hs.execute("~/Documents/app/bitbar/todo") ) )
 end)
 
--- not sure about this
+-- called on every config reload to notify if it was reloaded
 hs.alert.show("config loaded")
+
+-- move window left
+hs.hotkey.bind({"cmd"}, "F4", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+
+
+
+
 
 
