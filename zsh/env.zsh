c7465806ac15f45b0352508e4209a5dcd9dc6ef8
Nikita Voloboev <nikita.voloboev@gmail.com>
2018-11-29 23:20:03 +0100

    update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -10,12 +10,12 @@ export PATH=$PATH:~/.cargo/bin # Rust
 export GOPATH="$(go env GOPATH)" # Go
 export PATH="$PATH:$GOPATH/bin" # Go
 
-export PATH=$PATH:./node_modules/.bin/
+export PATH=$PATH:./node_modules/.bin
 
 # TODO: ?
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
-# TODO: change my pure prompt if I am in direnv env 
+# TODO: change my pure prompt if I am in direnv env
 # look at the existing prompt and use below function as it returns needed info
 has_direnv() { if [[ -n $DIRENV_DIFF ]]; then echo in direnv; else echo not; fi }
 
