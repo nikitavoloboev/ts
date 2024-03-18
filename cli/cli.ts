c5175824b6ecb127ad17d285388563119ef83bb6
Nikita <nikita.voloboev@gmail.com>
2024-03-18 18:44:09 +0400

    .

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,3 +1,6 @@
-// import { writeContentToDesktopFile } from "@nikitavoloboev/util"
-
-console.log()
+// TODO: improve on switch case in https://github.com/learn-anything/learn-anything.xyz/blob/dev/cmd.ts
+// make it so you can for given command get its help
+// or take the `//` before each case and generate a table in readme with possible commands and descriptions
+// probably better to wrap around https://github.com/effect-ts/effect/tree/main/packages/cli
+// example: https://github.com/IMax153/dadinator
+// make it nice/clean DX though
