0f652d140e820fab1b4dba8f28678d54c138d3f4
Nikita <nikita.voloboev@gmail.com>
2024-03-18 18:48:27 +0400

    .

--- cli/cli.ts
+++ cli/cli.ts
@@ -4,3 +4,14 @@
 // probably better to wrap around https://github.com/effect-ts/effect/tree/main/packages/cli
 // example: https://github.com/IMax153/dadinator
 // make it nice/clean DX though
+
+// in https://github.com/learn-anything/learn-anything.xyz/blob/dev/cmd.ts
+// there is this code
+// case undefined:
+//   console.log("No command provided")
+//   break
+// default:
+//   console.log("Unknown command")
+//   break
+// it should be automated away and instead show `--help` when wrong command or no command is entered
+// should be friendly UX
