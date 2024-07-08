9b67e4b9863ef41e2cb84ea3b13e26d2fe1c117b
Nikita <nikita.voloboev@gmail.com>
2024-07-08 11:10:27 +0200

    .

---
+++ scripts/base64-decode-to-file-from-clipboard.ts
@@ -0,0 +1,19 @@
+import { writeContentToDesktopFile } from "@nikiv/ts-utils"
+import { getClipboard } from "@nikiv/ts-utils/src/clipboard.js"
+import { isValidBase64 } from "@nikiv/ts-utils/src/other"
+
+// takes a base64 string from clipboard and writes it to a file
+// by default its `~/Desktop/base64-decoded.png`
+async function main() {
+  const clipboardContent = getClipboard()
+
+  if (!isValidBase64(clipboardContent)) {
+    console.error("Clipboard content is not a valid base64 string.")
+    return
+  }
+
+  const buffer = Buffer.from(clipboardContent, "base64")
+  writeContentToDesktopFile(buffer, "base64-decoded.png")
+}
+
+main()
